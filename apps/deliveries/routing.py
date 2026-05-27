d6e0c7b2e0876741eac4c0640b6929b7479e184c
franckdigital <franckalain.digital@gmail.com>
2026-05-27 16:26:28 +0000

    fix: downgrade cryptography to 46.0.0 for pyOpenSSL compatibility

--- apps/deliveries/routing.py
+++ apps/deliveries/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/delivery/(?P<tracking_number>[A-Z0-9]+)/$', consumers.DeliveryTrackingConsumer.as_asgi()),
+    re_path(r'ws/delivery/(?P<tracking_number>[A-Z0-9]+)/$', consumers.DeliveryTrackingConsumer.as_asgi()),
 ]
